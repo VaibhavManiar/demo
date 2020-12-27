f211702c4dd8a6cb4e61aa89ba866f3330874e23
vaibhav.m <vaibhavm@nova.tech>
2020-12-27 19:13:34 +0530

    Second Commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,5 +28,12 @@ public class DemoController {
   public String hello(@PathVariable("name") String name) {
     out.println("Free Main Thread : " + Thread.currentThread().getName());
     return service.hello(name);
+  }
+
+  @GetMapping("/api/async/gm/{name}")
+  @ResponseBody
+  public ListenableFuture<String> goodMorningAsync(@PathVariable("name") String name) {
+    out.println("Free Main Thread : " + Thread.currentThread().getName());
+    return service.greetingGoodMorningAsync(name);
   }
 }
--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,29 +1,54 @@
 package com.example.demo;
 
+import static java.lang.System.out;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
-
+import org.springframework.util.concurrent.ListenableFuture;
 @Service
 public class DemoService {
 
   @Async
   public CompletableFuture<String> helloAsync(String name) {
-    System.out.println("In hello async service : " + Thread.currentThread().getName());
+    out.println("In hello async service : " + Thread.currentThread().getName());
     try {
       Thread.sleep(1000);
-    } catch (InterruptedException ignore) {
-      ignore.printStackTrace();
+    } catch (Exception ignore) {
+
     }
     return CompletableFuture.completedFuture(hello(name))
         .thenApply((greetings) -> {
-          System.out.println(greetings + " : " + Thread.currentThread().getName());
+          out.println(greetings + " : " + Thread.currentThread().getName());
           return greetings;
         });
   }
 
   public String hello(String name) {
-    System.out.println("In hello service : " + Thread.currentThread().getName());
+    out.println("In hello service : " + Thread.currentThread().getName());
     return "Hello " + name;
+  }
+
+  @Async
+  public ListenableFuture<String> greetingGoodMorningAsync(String name) {
+    return AsyncResult.forValue(greetingGoodMorning(name));
+  }
+
+  public String greetingGoodMorning(String name) {
+    out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) +
+        " : In hello service -> greetingGoodMorning before : " + Thread.currentThread().getName());
+    try {
+      out.println("Sleep for 5 sec(s)");
+      Thread.sleep(5000);
+    } catch (Exception ignore) {
+
+    }
+    out.println("Awake after 5 sec(s)");
+    out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        + " : In hello service -> greetingGoodMorning after : " + Thread.currentThread().getName());
+    return "Good Morning " + name;
   }
 }
